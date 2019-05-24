0d0bdd925bc6a38e17f1c8605525256d68d35503
E <helloestar@gmail.com>
2019-05-25 02:36:41 +0800

    测试vscode源代码管理

--- 工程/Assets/E.Game/Scripts/MouseEvent.cs
+++ 工程/Assets/E.Game/Scripts/MouseEvent.cs
@@ -11,30 +11,21 @@ namespace E.Game
     {
         private void OnMouseEnter()
         {
-            GetComponent<SpriteRenderer>().color = new Color(0.8f, 0.8f, 0.8f);
         }
         private void OnMouseOver()
         {
-            if (Input.GetMouseButtonUp(1))
-            {
-                Debug.Log("右键点击了：" + name);
-            }
         }
         private void OnMouseDown()
         {
-            GetComponent<SpriteRenderer>().color = new Color(0.6f, 0.6f, 0.6f);
         }
         private void OnMouseDrag()
         {
-            
         }
         private void OnMouseUp()
         {
-            GetComponent<SpriteRenderer>().color = new Color(0.8f, 0.8f, 0.8f);
         }
         private void OnMouseExit()
         {
-            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1);
         }
     }
 }
